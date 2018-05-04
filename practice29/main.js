020236021311c157e3a51c0112819505fd54f87b
vokamura <vcokamura@gmail.com>
2018-05-04 10:51:44 -0700

    do math version 1

---
+++ practice29/main.js
@@ -0,0 +1,17 @@
+function do_math (num1, num2, operator) {
+    if (operator == "+") {
+        var addResult = num1 + num2;
+        return num1 + num2;
+    } else if (operator == "-") {
+        var subtractResult = num1 - num2;
+        return subtractResult;
+    } else if (operator == "*" || operator == "x" || operator == "X") {
+        var multiplyResult = num1 * num2;
+        return multiplyResult;
+    } else if (operator == "/") {
+        var divideResult = num1/num2;
+        return divideResult;
+    } else {
+        console.log("Did you enter 2 numbers and an operator?")
+    }
+}
